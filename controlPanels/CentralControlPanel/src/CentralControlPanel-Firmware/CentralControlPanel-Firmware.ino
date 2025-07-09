72a483079082d5c6bf926c6ef6cd3bbbdb9a763e
Luis Alejandro Bernal Romero <aztlek@GreenDurnak.aztlek.org>
2025-07-08 19:50:01 -0500

    Initial code documentation

--- controlPanels/CentralControlPanel/src/CentralControlPanel-Firmware/CentralControlPanel-Firmware.ino
+++ controlPanels/CentralControlPanel/src/CentralControlPanel-Firmware/CentralControlPanel-Firmware.ino
@@ -1,5 +1,52 @@
+/*
+||
+|| @file CentralControlPanel-Firmware
+|| @author Luis Alejandro Bernal Romero (Aztlek)
+|| @description
+|| | This firmware is for the "Central Control Panel" of the "Modular Cockpit".
+|| | Multiple Teensy joysticks can be connected to the pseudohub, and it 
+|| | unifies them into a single one so that the operating system only sees 
+|| | one joystick. This is done through a USB hub connected to a Teensy 4.1 
+|| | via the "USB Host" port.
+|| | @see Power Module: https://github.com/aztlek/ModularCockpit/tree/main/controlPanels/CentralControlPanel
+|| | @see Modular Cockpit: https://github.com/aztlek/ModularCockpit.
+|| #
+|| 
+|| @license
+|| | Copyright (C) 2020 Luis Alejandro Bernal Romero (Aztlek)
+|| | 
+|| | This program is free software: you can redistribute it and/or modify
+|| | it under the terms of the GNU General Public License as published by
+|| | the Free Software Foundation, either version 3 of the License, or
+|| | (at your option) any later version.
+|| | 
+|| | This program is distributed in the hope that it will be useful,
+|| | but WITHOUT ANY WARRANTY; without even the implied warranty of
+|| | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|| | GNU General Public License for more details.
+|| | 
+|| | You should have received a copy of the GNU General Public License
+|| | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+|| #
+||
+*/
+
 
 #include "USBHost_t36.h"
+
+// Configuration
+//===============
+
+/*
+Here you should enter the total number of keys for each module. The order 
+matters, and it must be the order in which they are connected to the USB hub.
+
+If you have any doubts about the connection order, you can uncomment the
+ DEBUG line, and the Arduino IDE serial console will display the number 
+ of each module when you press the keys.
+*/
+
+#define DEBUG
 
 int buttons_per_joystick[] = {
   12, // Torret module
@@ -52,7 +99,10 @@ void setup()
 {
   Serial1.begin(2000000);
   while (!Serial) ;
+
+#ifdef DEBUG
   Serial.println("\n\nUSB Host Joystick Testing");
+#endif  
   myusb.begin();
 }
 
@@ -65,15 +115,23 @@ void loop()
     offset += ((joystick_index == 0)? 0 : buttons_per_joystick[joystick_index - 1]);
     if (joysticks[joystick_index].available()) {
       uint32_t buttons = joysticks[joystick_index].getButtons();
+#ifdef DEBUG      
       Serial.printf ("Joystick(%2d): ", joystick_index);
+#endif      
       int num_buttons = 0;
       int *arr_buttons = getPressedButtons(buttons, num_buttons);
+#ifdef DEBUG      
       Serial.printf("num_buttons = %d buttons = ", num_buttons);
+#endif      
       for(int b = 0; b < num_buttons; b++) {
         int relative_button = arr_buttons[b] + offset;
+#ifdef DEBUG        
         Serial.printf("%d(%d) ", arr_buttons[b], relative_button);
+#endif        
       }
+#ifdef DEBUG      
       Serial.println();
+#endif      
       joysticks[joystick_index].joystickDataClear();
     }
   }
